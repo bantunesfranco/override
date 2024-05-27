94b4480601b41aa16d094a2962959e745c62903c
Bruno Franco <brunoantunesfranco@gmail.com>
2024-05-27 13:59:07 +0200

    first commit

---
+++ level00/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	puts("***********************************");
+	puts("* \t     -Level00 -\t\t  *");
+	puts("***********************************");
+	printf("Password:");
+	scanf("%d", &)
+
+}
