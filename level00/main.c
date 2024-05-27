ed892a1eeac63ed294570d13173618e5a2d11abb
Bruno Franco <bfranco@student.codam.nl>
2024-05-27 15:15:20 +0200

    level00 flag

--- level00/main.c
+++ level00/main.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main(void)
 {
+	int	password;
+
 	puts("***********************************");
 	puts("* \t     -Level00 -\t\t  *");
 	puts("***********************************");
 	printf("Password:");
-	scanf("%d", &)
 
+	scanf("%d", &password);
+
+	if (password == 5276)
+	{
+		puts("\nAuthenticated!");
+		system("/bin/sh");
+		return (0);
+	}
+	else
+	{
+		puts("\nInvalid Password!");
+		return (1);
+	}
 }
